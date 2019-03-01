72a04ade8c47bc871086736e13bf626a6cab2351
daniel.mbarbosa1 <daniel.mbarbosa1@CAS511892W1016.senacsp.edu.br>
2019-02-28 21:26:13 -0300

    Versão inicial do execício

---
+++ geren-prod/src/main/java/br/senac/tads/pi3/geren/prod/ProdutoDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.geren.prod;
+
+/**
+ *
+ * @author daniel.mbarbosa1
+ */
+class ProdutoDAO {
+
+    void listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
